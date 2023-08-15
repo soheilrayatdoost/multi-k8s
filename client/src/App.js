948250a3d9bfa258c30cf3591c7e9084cec724a8
Soheil Rayatdoost <srayatdo@lab4tech.lan>
2023-08-15 13:31:01 +0200

    changed main link

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.linkedin.com/in/soheil-rayatdoost/"
             target="_blank"
             rel="noopener noreferrer"
           >
