cb14985742ad9fbfc70a182423e63c08188f7ab7
Soheil Rayatdoost <srayatdo@lab4tech.lan>
2023-08-25 13:22:05 +0200

    change project to K8s

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Ya ali
+            bayad berim az inja
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
