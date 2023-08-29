49e84fea4fc7869a38aa5aa54efb5786f4d0f284
Soheil Rayatdoost <srayatdo@lab4tech.lan>
2023-08-29 16:23:49 +0200

    fixed updating container in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker push soheilr/multi-server:$SHA
 docker push soheilr/multi-worker:$SHA
 
 kubectl apply -f ./k8s
-kubectl set image deployment/client-deployment client-soheilr/multi-client:$SHA
-kubectl set image deployment/server-deployment server-soheilr/multi-server:$SHA
-kubectl set image deployment/worker-deployment worker-soheilr/multi-worker:$SHA
+kubectl set image deployment/client-deployment client=soheilr/multi-client:$SHA
+kubectl set image deployment/server-deployment server=soheilr/multi-server:$SHA
+kubectl set image deployment/worker-deployment worker=soheilr/multi-worker:$SHA
